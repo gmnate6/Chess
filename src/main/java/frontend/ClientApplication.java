e8a9c65f640b1c2483da149842b484537b71c4e9
Nathan Holmberg <gmnate6@gmail.com>
2025-04-14 15:31:38 -0400

    Created a splash screen while assets and settings are loading in

--- src/main/java/frontend/ClientApplication.java
+++ src/main/java/frontend/ClientApplication.java
@@ -4,20 +4,39 @@ import frontend.controller.MainController;
 import frontend.model.SettingsManager;
 import frontend.model.assets.AssetManager;
 import frontend.view.MainFrame;
+import frontend.view.utils.SplashScreen;
 
 import javax.swing.*;
 
 public class ClientApplication {
     public static void main(String[] args) {
-        // Initialize Assets & Settings
-        AssetManager.initialize();
-        SettingsManager.initialize();
-
-        // Start Application
         SwingUtilities.invokeLater(() -> {
-            MainFrame mainFrame = new MainFrame();
-            new MainController(mainFrame);
-            mainFrame.setVisible(true);
+            // Splash Screen
+            SplashScreen splash = new SplashScreen();
+            splash.setVisible(true);
+
+            // Use a SwingWorker to perform long-running initialization in a background thread.
+            new SwingWorker<Void, Void>() {
+                @Override
+                protected Void doInBackground() {
+                    // Initialize Assets & Settings
+                    AssetManager.initialize();
+                    SettingsManager.initialize();
+                    return null;
+                }
+
+                @Override
+                protected void done() {
+                    // Dispose of the splash screen.
+                    splash.dispose();
+
+                    // Start the main application.
+                    MainFrame mainFrame = new MainFrame();
+                    mainFrame.setLocationRelativeTo(null);
+                    new MainController(mainFrame);
+                    mainFrame.setVisible(true);
+                }
+            }.execute();
         });
     }
 }
